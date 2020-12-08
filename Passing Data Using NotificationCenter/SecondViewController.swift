8e86b2678fb68d0ec95160a202661d5fe7c4483e
MannaICT13 <khmannaict13@gmail.com>
2020-12-08 20:42:49 +0600

    created ui and there outlets ,action

---
+++ Passing Data Using NotificationCenter/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Passing Data Using NotificationCenter
+//
+//  Created by Md Khaled Hasan Manna on 8/12/20.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var myImg: UIImageView!
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    
+}
+
--- Passing Data Using NotificationCenter/ViewController.swift
+++ Passing Data Using NotificationCenter/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func mananBtnAction(_ sender: Any) {
+    }
+    
+    @IBAction func khaledBtnAction(_ sender: Any) {
+    }
 }
 
