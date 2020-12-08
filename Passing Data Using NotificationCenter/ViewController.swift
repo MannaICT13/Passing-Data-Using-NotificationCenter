0ce887f297e5349ac3bde2dce488a3f4419b7120
MannaICT13 <khmannaict13@gmail.com>
2020-12-08 20:30:38 +0600

    Initial Commit

---
+++ Passing Data Using NotificationCenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Passing Data Using NotificationCenter
+//
+//  Created by Md Khaled Hasan Manna on 8/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
