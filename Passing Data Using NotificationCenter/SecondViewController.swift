fcec95bbf3c948eea0f20288c50b8ea20a820589
MannaICT13 <khmannaict13@gmail.com>
2020-12-10 04:22:23 +0600

    completed implementation using NotificationCenter post method

--- Passing Data Using NotificationCenter/SecondViewController.swift
+++ Passing Data Using NotificationCenter/SecondViewController.swift
@@ -2,24 +2,30 @@
 //  SecondViewController.swift
 //  Passing Data Using NotificationCenter
 //
-//  Created by Md Khaled Hasan Manna on 8/12/20.
+//  Created by Md Khaled Hasan Manna on 10/12/20.
 //
 
 import UIKit
 
 class SecondViewController: UIViewController {
-    
-    @IBOutlet weak var myImg: UIImageView!
-    
-    @IBOutlet weak var nameLbl: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+    
+    }
+    //MARK:- Handlers
+    
+
+    @IBAction func mannaBtnAction(_ sender: Any) {
+        NotificationCenter.default.post(name: .manna, object: nil)
+        
+        
     }
     
-
+    @IBAction func khaledBtnAction(_ sender: Any) {
+        NotificationCenter.default.post(name: .khaled, object: nil)
+        
+    }
     
 }
-
--- Passing Data Using NotificationCenter/ViewController.swift
+++ Passing Data Using NotificationCenter/ViewController.swift
@@ -2,22 +2,51 @@
 //  ViewController.swift
 //  Passing Data Using NotificationCenter
 //
-//  Created by Md Khaled Hasan Manna on 8/12/20.
+//  Created by Md Khaled Hasan Manna on 10/12/20.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    //MARK:-Properties
+    
+    @IBOutlet weak var myImg: UIImageView!
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next->", style: .done, target: self, action: #selector(next(_:)))
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(manna(_:)), name: .manna, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(khaled(_:)), name: .khaled, object: nil)
 
-    @IBAction func mananBtnAction(_ sender: Any) {
+        
+    }
+    @objc func next(_ sender : UIBarButtonItem){
+        let sVC = self.storyboard?.instantiateViewController(identifier: "SecondViewController") as! SecondViewController
+        self.navigationController?.pushViewController(sVC, animated: true)
     }
     
-    @IBAction func khaledBtnAction(_ sender: Any) {
+    @objc func manna(_ sender: NotificationCenter){
+        myImg.image = #imageLiteral(resourceName: "Manna copy 2")
+        nameLbl.text = "Manna"
     }
+    @objc func khaled(_ sender: NotificationCenter){
+        myImg.image = #imageLiteral(resourceName: "Manna copy")
+        nameLbl.text = "Khaled"
+    }
+    
+
+   
+
 }
 
+extension Notification.Name{
+    
+    
+    static let manna = Notification.Name(rawValue: "manna")
+    static let khaled = Notification.Name(rawValue: "khaled")
+}
